9bbda94f4c3880541f011610b1ceea8fa033fee2
Sherif Khedr <shrief.issawy@gmail.com>
2021-11-12 19:20:18 +0200

    updating Hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,19 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
-);
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Hi, I'm Sherif <br />
+        Frontend web dev
+      </SectionTitle>
+      <SectionText>
+        I help customers all over the world build their web apps. 
+        Specilaized in creating UI with React and can provide limited Backend services using Firebase.
+      </SectionText>
+      <Button onClick={props.handleClick}>Learn More</Button>
+    </LeftSection>
+  </Section>
+)
 
 export default Hero;
